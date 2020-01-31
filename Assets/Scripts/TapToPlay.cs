96afdd200dc66f6f298436fefc0606299ef8accf
Paul Martin <pmartin36@gmail.com>
2020-01-30 23:37:41 -0500

    logo and icon

---
+++ Assets/Scripts/TapToPlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class TapToPlay : MonoBehaviour
+{
+	private TMP_Text text;
+	public float Power;
+
+    void Start() {
+		text = GetComponent<TMP_Text>();
+    }
+
+    void Update() {
+		text.color = new Color(1, 1, 1, Power + 0.1f);
+		text.fontMaterial.SetFloat("_GlowOuter", Power - 0.3f);
+    }
+}
